342c273009ef153a691f8afdc107fadfbbf99e58
chilledcatcoin-cmd <chilledcatcoin@gmail.com>
2025-10-09 19:42:25 -0230

    Update contests.js

--- features/contests/contests.js
+++ features/contests/contests.js
@@ -5,7 +5,7 @@
  */
 
 const { GAMES } = require("../games");
-const { getLeaderboardCached, getStatName } = require("../leaderboard/leaderboard");
+const { getLeaderboardCached, getStatName } = require("../leaderboard/");
 const { isWhitelisted } = require("../../modules/safecat/protector");
 
 const contests = new Map();
